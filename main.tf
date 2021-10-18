6943a04180d9d8ef0e68cf716c9a1014c116c705
dhlee011 <83205371+dhlee011@users.noreply.github.com>
2021-10-18 16:23:18 +0900

    Update main.tf
    차후 변수 설정 진행

--- main.tf
+++ main.tf
@@ -171,3 +171,68 @@ resource "aws_route_table" "seoul_idc_rt" {
 resource "aws_route_table_association" "route_table_association_seoul_idc" {
   subnet_id = aws_subnet.seoul_idc_subnet1.id
   route_table_id = aws_route_table.seoul_idc_rt.id
+
+  
+  resource "aws_customer_gateway" "cgw" {
+  bgp_asn    = 65000
+  ip_address = "52.79.153.38"
+  type       = "ipsec.1"
+
+  tags = {
+    Name = "seoul-idc-cgw"
+  }
+}
+
+resource "aws_ec2_transit_gateway" "seoul_tgw" {
+  description = "seoul_tgw"
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "seoul_tgw_vpc" {
+  subnet_ids         = [aws_subnet.seoul_TGW_subnet1.id, aws_subnet.seoul_TGW_subnet1.id]
+  transit_gateway_id = aws_ec2_transit_gateway.seoul_tgw.id
+  vpc_id             = aws_vpc.seoul.id
+
+  tags = {
+    name = "seoul-tgw-vpc"
+
+  }
+}
+
+
+resource "aws_vpn_connection" "seoul_tgw_vpn" {
+  customer_gateway_id = aws_customer_gateway.cgw.id
+  transit_gateway_id  = aws_ec2_transit_gateway.seoul_tgw.id
+  type                = aws_customer_gateway.cgw.type
+
+  static_routes_only = true
+
+  tunnel1_preshared_key = "cloudneta"
+
+  tunnel2_preshared_key = "cloudneta"
+
+
+}
+
+data "aws_ec2_transit_gateway_vpn_attachment" "seoul-cgw-tgw" {
+  transit_gateway_id = aws_ec2_transit_gateway.seoul_tgw.id
+  vpn_connection_id  = aws_vpn_connection.seoul_tgw_vpn.id
+}
+
+
+resource "aws_ec2_transit_gateway_route" "seoul_tgw_route" {
+  destination_cidr_block         = "10.0.0.0/8"
+  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_vpn_attachment.seoul-cgw-tgw.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.seoul_tgw.association_default_route_table_id
+}
+
+
+
+resource "aws_ec2_transit_gateway_peering_attachment" "seoul-peering-singapore" {
+  peer_region             = "ap-southeast-1"
+  peer_transit_gateway_id = "tgw-0ebd2201f0841cd50"
+  transit_gateway_id      = "tgw-09f51ee7218ad7241"
+
+  tags = {
+    Name = "seoul-peering-singapore"
+  }
+}
