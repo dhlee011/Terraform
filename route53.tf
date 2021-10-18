d5649feb5d5f65e9f433bfe24699218531abec59
DONGHAELEE <83205371+dhlee011@users.noreply.github.com>
2021-10-18 16:41:37 +0900

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -20,3 +20,34 @@ resource "aws_route53_zone_association" "seoul_attch" {
   zone_id = aws_route53_zone.seoul_zone.zone_id
   vpc_id  = aws_vpc.seoul.id
 }
+
+
+
+
+
+
+
+
+
+resource "aws_route53_zone" "singapore_zone" {
+  name = "aws-singapore.interal"
+
+  vpc {
+    vpc_id = aws_vpc.singapore.id
+  }
+}
+
+
+resource "aws_route53_record" "A_seoul" {
+  zone_id = aws_route53_zone.singapore_zone.zone_id
+  name    = "web1"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.3.3.100"]
+}
+
+
+resource "aws_route53_zone_association" "singapore_attch" {
+  zone_id = aws_route53_zone.singapore_zone.zone_id
+  vpc_id  = aws_vpc.singapore.id
+}
