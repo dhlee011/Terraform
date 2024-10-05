2eddf8c5b9126d5493115ec047def25157b8edb6
DongHae <dlehdgo011@naver.com>
2024-10-05 17:28:02 +0900

    Moudule_v1

---
+++ Network/variable.tf
@@ -0,0 +1,29 @@
+
+variable "vpcs" {
+  type = list(string)
+}
+
+variable "vpcs_cidr" {
+  type = list(string)
+}
+
+variable "vpcs_names" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "subnets_cidr" {
+  type = list(string)
+}
+
+variable "subnets_names" {
+  type = list(string)
+}
+
